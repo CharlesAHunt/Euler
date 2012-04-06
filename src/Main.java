bf69be0ed8e81a8134ce08aecec3cb54ee5fd894
Charles Hunt <charles-hunt@uiowa.edu>
2012-04-05 22:42:35 -0500

    solution to problem 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * User: Charles
+ * Date: 4/5/12
+ */
+public class Main {
+
+    public static void main(String [] args){
+
+        runProblemSets();
+
+    }
+
+    private static void runProblemSets() {
+        System.out.println("Running Problem Sets...");
+        System.out.println("Answer to problem 1: " + Problem1.solve());
+    }
+}
