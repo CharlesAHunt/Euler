16bd03c1cf5541965b1d2b5095ab0cc69856d965
Charles Hunt <charles-hunt@uiowa.edu>
2012-07-28 23:00:27 -0500

    problem 7 init

---
+++ src/Problem7.java
@@ -0,0 +1,19 @@
+/**
+ * User: Charles
+ * Date: 7/28/12
+ *
+
+ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+ What is the 10001st prime number?
+ *
+ */
+public class Problem7 implements Solveable {
+
+    public Double solve() {
+
+        return 0.0;   //todo solve
+
+    }
+
+}
