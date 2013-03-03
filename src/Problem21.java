b9d2eef81d5d25e7ec4f640de03da28e4a8ec16f
CharlesAHunt <charles-hunt@uiowa.edu>
2013-03-03 00:54:19 -0600

    solution to problem 21

--- src/Problem21.java
+++ src/Problem21.java
@@ -8,7 +8,8 @@ import java.util.Map;
  * Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
  If d(a) = b and d(b) = a, where a  b, then a and b are an amicable pair and each of a and b are called amicable numbers.
 
- For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+ For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
  Evaluate the sum of all the amicable numbers under 10000.
  */
@@ -18,23 +19,32 @@ public class Problem21 implements Solveable {
     public Long solve() {
 
         Long result = 0L;
-        Map<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> principalSumMap = new HashMap<Integer, Integer>();
 
-        for(int number = 1; number <= 10000; number++) {
-
-            numberMap.put(1, getDivisorsSum(number));
-
+        for(int number = 1; number < 10000; number++) {
+            principalSumMap.put(number, getDivisorsSum(number));
         }
 
-        //todo go through the map and find the matches, sum up
+        for(Integer key : principalSumMap.keySet()) {
+            for(Integer valueKey : principalSumMap.keySet()) {
+                if(key.intValue() == principalSumMap.get(valueKey).intValue() && principalSumMap.get(key).intValue() == valueKey && key.intValue() != valueKey.intValue())
+                    result += key;
+            }
+        }
 
         return result;
 
     }
 
-    private int getDivisorsSum(Integer number) {
-       //todo implement this
-       return 0;
+    private Integer getDivisorsSum(Integer number) {
+        int sum = 0;
+
+        for(int divisor = 1; divisor <= (number/2); divisor++ ) {
+            if(number % divisor == 0)
+                sum += divisor;
+        }
+
+        return sum;
     }
 
 }
