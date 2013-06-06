8ecad6be3adcb1bc34bdfbe4200cf6d6d22f3e5e
Charles Hunt <charles-hunt@uiowa.edu>
2013-06-05 21:01:05 -0500

    problem 22 solution

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,28 @@ public class Main {
 
         show("Running problem sets...");
 
-        show("Answer to problem 1: " + new Problem1().solve());
-        show("Answer to problem 2: " + new Problem2().solve());
-        show("Answer to problem 3: " + new Problem3().solve());
-        show("Answer to problem 4: " + new Problem4().solve());
-        show("Answer to problem 5: " + new Problem5().solve());
-        show("Answer to problem 6: " + new Problem6().solve());
-        show("Answer to problem 7: " + new Problem7().solve());
-        show("Answer to problem 8: " + new Problem8().solve());
-        show("Answer to problem 9: " + new Problem9().solve());
-        show("Answer to problem 10: " + new Problem10().solve());
-        show("Answer to problem 11: " + new Problem11().solve());
-        show("Answer to problem 12: " + new Problem12().solve());
-        show("Answer to problem 13: " + new Problem13().solve());
-        show("Answer to problem 14: " + new Problem14().solve());
-        show("Answer to problem 15: " + new Problem15().solve());
-        show("Answer to problem 16: " + new Problem16().solve());
-        show("Answer to problem 17: " + new Problem17().solve());
-        show("Answer to problem 18: " + new Problem18().solve());
-        show("Answer to problem 19: " + new Problem19().solve());
-        show("Answer to problem 20: " + new Problem20().solve());
-        show("Answer to problem 21: " + new Problem21().solve());
+//        show("Answer to problem 1: " + new Problem1().solve());
+//        show("Answer to problem 2: " + new Problem2().solve());
+//        show("Answer to problem 3: " + new Problem3().solve());
+//        show("Answer to problem 4: " + new Problem4().solve());
+//        show("Answer to problem 5: " + new Problem5().solve());
+//        show("Answer to problem 6: " + new Problem6().solve());
+//        show("Answer to problem 7: " + new Problem7().solve());
+//        show("Answer to problem 8: " + new Problem8().solve());
+//        show("Answer to problem 9: " + new Problem9().solve());
+//        show("Answer to problem 10: " + new Problem10().solve());
+//        show("Answer to problem 11: " + new Problem11().solve());
+//        show("Answer to problem 12: " + new Problem12().solve());
+//        show("Answer to problem 13: " + new Problem13().solve());
+//        show("Answer to problem 14: " + new Problem14().solve());
+//        show("Answer to problem 15: " + new Problem15().solve());
+//        show("Answer to problem 16: " + new Problem16().solve());
+//        show("Answer to problem 17: " + new Problem17().solve());
+//        show("Answer to problem 18: " + new Problem18().solve());
+//        show("Answer to problem 19: " + new Problem19().solve());
+//        show("Answer to problem 20: " + new Problem20().solve());
+//        show("Answer to problem 21: " + new Problem21().solve());
+        show("Answer to problem 22: " + new Problem22().solve());
 
         show("Problem sets solved.");
 
