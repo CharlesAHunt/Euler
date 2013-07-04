655a1d4746c3439c50589a3d7f9724f94913a1a9
Charles Hunt <charlesalberthunt@gmail.com>
2013-07-03 22:52:00 -0500

    prob 24 init

---
+++ src/Problem24.java
@@ -0,0 +1,15 @@
+/**
+ * User: Charles
+ * Date: 7/3/13
+ */
+public class Problem24 implements Solveable {
+
+    public Long solve() {
+
+        Long result = 0L;
+
+        return result;
+
+    }
+
+}
