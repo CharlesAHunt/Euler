c7b71eb7dafc3d8930232226e4f46e34a9b7b475
Charles Hunt <charlesalberthunt@gmail.com>
2014-01-06 23:16:07 -0600

    partial solution to 30

---
+++ src/Problem30.scala
@@ -0,0 +1,29 @@
+/**
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+ */
+
+class Problem30 {
+
+  def solve(): String = {
+
+    var sumOfFifthNumbers: List[Int] = List()
+
+    for(j <- 2 to 999999) {
+      if((Math.pow(j.toString.charAt(0).toInt,5))+ (Math.pow(j.toString.charAt(1).toInt,5)) + (Math.pow(j.toString.charAt(2).toInt,5)) + (Math.pow(j.toString.charAt(3).toInt,5)) == j )
+        sumOfFifthNumbers = j :: sumOfFifthNumbers
+    }
+
+    sumOfFifthNumbers.foldLeft(0)(_+_).toString
+
+  }
+
+}
